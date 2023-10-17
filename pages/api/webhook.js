0e967c55c18bafa469025053aeaf3aa3548fb0e4
Arifm6 <arifmassih6@gmail.com>
2023-10-17 00:22:57 -0400

    Updated firebase private key
    Generated new firebase private keys and stored in env variables

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -3,11 +3,18 @@ import * as admin from "firebase-admin";
 const { getFirestore } = require("firebase-admin/firestore");
 
 //secure connection to firebase from our backend.
-const publicServiceAccount = require("../../permissions.json");
 const serviceAccount = {
-  ...publicServiceAccount,
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
 };
 
 const app = !admin.apps.length
